5d60fafb67465f5031cb2bc765dd48a97ec188b4
Wojciech Bednarski <wojciech.bednarski.wb@gmail.com>
2024-04-15 19:48:54 +0200

    Added new class and code

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,7 +1,11 @@
+package com.kodilla.testing.collection;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbersExterminator {
+
+
 
     public List<Integer> exterminate(List<Integer> numbers) {
         List<Integer> evenNumbers = new ArrayList<>();
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,9 @@
 package com.kodilla.testing.collection;
 
+import java.util.List;
+
 public class OddNumbersExterminator {
+    public List<Integer> exterminate(List<Integer> emptyList) {
+        return emptyList;
+    }
 }

ba277e64cbe73a0e9bb8e65ce578373914f75e4c
Wojciech Bednarski <wojciech.bednarski.wb@gmail.com>
2024-04-18 18:33:48 +0200

    Added new class and code

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
@@ -1,41 +1,12 @@
-package com.kodilla.testing;
-
-import com.kodilla.testing.calculator.Calculator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+package com.kodilla.testing.calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        CalculatorTest test = new CalculatorTest();
-        test.testAdd();
-        test.testSubtract();
+        Calculator calculator = new Calculator();
+        int result1 = calculator.add(5, 3);
+        int result2 = calculator.subtract(5, 3);
+        System.out.println(result1);
+        System.out.println(result2);
+
     }
-
-
-
-    public class CalculatorTest {
-
-        @Test
-        public void testAdd() {
-            // Given
-            Calculator calculator = new Calculator();
-
-            // When
-            int result = calculator.add(5, 3);
-
-            // Then
-            Assertions.assertEquals(8, result);
-        }
-
-        @Test
-        public void testSubtract() {
-            // Given
-            Calculator calculator = new Calculator();
-
-            // When
-            int result = calculator.subtract(5, 3);
-
-            // Then
-            Assertions.assertEquals(2, result);
-        }
-    }
+}

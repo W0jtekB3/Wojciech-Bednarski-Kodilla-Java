d7cc6b218cd8af3ebddcc779da183ebe12c26a18
Wojciech Bednarski <wojciech.bednarski.wb@gmail.com>
2024-05-12 13:00:04 +0200

    Added new class and code

--- kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
@@ -1,14 +1,7 @@
 package com.kodilla.spring;
 
-import com.kodilla.spring.shape.Shape;
-import com.kodilla.spring.shape.Square;
-import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootApplication
 public class KodillaSpringApplication {
@@ -17,15 +10,4 @@ public class KodillaSpringApplication {
         SpringApplication.run(KodillaSpringApplication.class, args);
     }
 
-    @Test // Use the correct Test annotation
-    public void testSquareLoadedIntoContainer() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Shape shape = (Shape) context.getBean(Square.class);
-        //When
-        String name = ((Square) shape).getShapeName();
-        //Then
-        assertEquals("This is a square.", name);
-    }
 }
